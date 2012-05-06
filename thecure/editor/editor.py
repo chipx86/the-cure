18e78dadefce6e895db9cd8563ea2c13a6d0364f
Christian Hammond <chipx86@chipx86.com>
2012-05-06 02:36:49 -0700

    Added level saving and sprite shifting to the editor.
    Levels can now be saved, and are loaded again properly.
    
    The sprites can now be shifted in the X or Y direction by half their
    size, taking advantage of the tiling aspects of some of the sprites
    I'm using.

--- thecure/editor/editor.py
+++ thecure/editor/editor.py
@@ -14,6 +14,7 @@ except:
 
 from thecure.levels import get_levels
 from thecure.levels.loader import LevelLoader
+from thecure.levels.writer import LevelWriter
 from thecure.resources import get_image_filename, get_level_filename
 from thecure.sprites import Tile
 
@@ -46,6 +47,9 @@ def _load_tile(name, x, y):
 
 
 class LevelGrid(gtk.DrawingArea):
+    # XXX Hard-coding these is fragile.
+    LAYERS = ['bg', 'main', 'fg']
+
     def __init__(self, tile_list):
         super(LevelGrid, self).__init__()
 
@@ -58,6 +62,10 @@ class LevelGrid(gtk.DrawingArea):
         self.gc = None
         self.image = None
         self.cursor_area = None
+        self.current_layer = None
+        self.loaded = False
+        self.drawing = False
+        self.tiles = {}
 
         self.connect('expose-event', self._on_expose_event)
         self.connect('button-press-event', self._on_button_press)
@@ -68,25 +76,74 @@ class LevelGrid(gtk.DrawingArea):
         self.realize()
 
         style = self.get_style()
+        self.loader = loader
         self.gc = style.fg_gc[gtk.STATE_NORMAL]
         self.bg_gc = self.style.bg_gc[gtk.STATE_NORMAL]
         self.cursor_gc = self.style.bg_gc[gtk.STATE_SELECTED]
+        self.current_layer = len(self.LAYERS) - 1
 
+        for layer_name in self.LAYERS:
+            self.tiles[layer_name] = [
+                [None] * loader.get_width()
+                for i in range(loader.get_height())
+            ]
+
+        self._reload_layers()
+        self.loaded = True
+
+    def write(self, writer):
+        writer.write([
+            {
+                'name': layer_name,
+                'is_main': layer_name == 'main',
+                'tiles': self.tiles[layer_name]
+            }
+            for layer_name in self.LAYERS
+        ])
+
+    def _clear(self):
         self.image = gtk.gdk.Pixmap(self.window,
-                                    loader.get_width() * Tile.WIDTH,
-                                    loader.get_height() * Tile.HEIGHT)
+                                    self.loader.get_width() * Tile.WIDTH,
+                                    self.loader.get_height() * Tile.HEIGHT)
 
-        for tile_data in loader.iter_tiles('bg'):
+    def _load_layer(self, layer_name):
+        layer_tiles = self.tiles[layer_name]
+
+        for tile_data in self.loader.iter_tiles(layer_name):
             tilesheet = _load_tilesheet(tile_data['tile_file'])
+            row = tile_data['row']
+            col = tile_data['col']
+
+            if not self.loaded:
+                layer_tiles[row][col] = {
+                    'filename': tile_data['tile_file'],
+                    'tile_x': tile_data['tile_x'],
+                    'tile_y': tile_data['tile_y'],
+                }
 
             self.image.draw_pixbuf(self.gc,
                                    tilesheet,
-                                   tile_data['tile_x'] * Tile.WIDTH,
-                                   tile_data['tile_y'] * Tile.HEIGHT,
-                                   tile_data['col'] * Tile.WIDTH,
-                                   tile_data['row'] * Tile.HEIGHT,
+                                   int(tile_data['tile_x'] * Tile.WIDTH),
+                                   int(tile_data['tile_y'] * Tile.HEIGHT),
+                                   col * Tile.WIDTH,
+                                   row * Tile.HEIGHT,
                                    Tile.WIDTH,
                                    Tile.HEIGHT)
+
+    def set_current_layer(self, index):
+        if index != self.current_layer:
+            self.current_layer = index
+
+            if self.loaded:
+                self._reload_layers()
+
+    def _reload_layers(self):
+        self._clear()
+
+        for i in range(self.current_layer + 1):
+            self._load_layer(self.LAYERS[i])
+
+        self.queue_draw()
 
     def _on_expose_event(self, level_grid, e):
         if self.image:
@@ -98,9 +155,13 @@ class LevelGrid(gtk.DrawingArea):
                 self.window.draw_rectangle(self.cursor_gc, False,
                                            *self.cursor_area)
 
-    def _on_button_press(self, w, e):
+    def _place_tile(self, e):
         tile_area = self._get_tile_area(e)
+        tile_x = tile_area[0] / Tile.WIDTH
+        tile_y = tile_area[1] / Tile.HEIGHT
         selected_tile = self.tile_list.selected_tile
+
+        tiles = self.tiles[self.LAYERS[self.current_layer]]
 
         if selected_tile:
             pixbuf = selected_tile['pixbuf']
@@ -109,15 +170,30 @@ class LevelGrid(gtk.DrawingArea):
                                    0,
                                    0,
                                    *tile_area)
+
+            tiles[tile_y][tile_x] = {
+                'filename': selected_tile['filename'],
+                'tile_x': selected_tile['x'],
+                'tile_y': selected_tile['y'],
+            }
         else:
             self.image.draw_rectangle(self.bg_gc, True, *tile_area)
+            tiles[tile_y][tile_x] = None
 
         self.queue_draw_area(*tile_area)
 
+    def _on_button_press(self, w, e):
+        self._place_tile(e)
+        self.drawing = True
+
     def _on_button_release(self, w, e):
-        pass
+        self.drawing = False
 
     def _on_motion_notify(self, w, e):
+        if self.drawing:
+            self._place_tile(e)
+            return
+
         old_cursor_area = self.cursor_area
         self.cursor_area = self._get_tile_area(e)
 
@@ -152,17 +228,23 @@ class TileList(gtk.Table):
         self._group = None
         self.selected_button = None
         self.selected_tile = None
+        self.shift_x = False
+        self.shift_y = False
 
         self.set_filename('ground_1.png')
 
     def set_filename(self, filename):
-        pixbuf = _load_tilesheet(filename)
-        assert pixbuf
+        self.filename = filename
+        self.pixbuf = _load_tilesheet(filename)
+        assert self.pixbuf
 
-        tiles_x = pixbuf.get_width() / Tile.WIDTH
-        tiles_y = pixbuf.get_height() / Tile.HEIGHT
+        self._reload_tiles()
 
-        self.resize(0, 0)
+    def _reload_tiles(self):
+        tiles_x = self.pixbuf.get_width() / Tile.WIDTH
+        tiles_y = self.pixbuf.get_height() / Tile.HEIGHT
+
+        self.foreach(lambda w: w.destroy())
 
         # Add the "erase" one
         button = self._add_button(0, 0)
@@ -176,8 +258,24 @@ class TileList(gtk.Table):
         image.set_size_request(Tile.WIDTH, Tile.HEIGHT)
 
         for y in range(tiles_y):
+            attach_y = y
+
+            if self.shift_y:
+                if y == tiles_y - 1:
+                    break
+
+                y += 0.5
+
             for x in range(tiles_x):
-                tile_pixbuf = _load_tile(filename, x, y)
+                attach_x = x
+
+                if self.shift_x:
+                    if x == tiles_x - 1:
+                        break
+
+                    x += 0.5
+
+                tile_pixbuf = _load_tile(self.filename, x, y)
 
                 if not tile_pixbuf:
                     continue
@@ -185,14 +283,22 @@ class TileList(gtk.Table):
                 image = gtk.image_new_from_pixbuf(tile_pixbuf)
                 image.show()
 
-                button = self._add_button(y + 1, x, {
-                    'filename': filename,
+                button = self._add_button(attach_y + 1, attach_x, {
+                    'filename': self.filename,
                     'x': x,
                     'y': y,
-                    'tilesheet': pixbuf,
+                    'tilesheet': self.pixbuf,
                     'pixbuf': tile_pixbuf,
                 })
                 button.add(image)
+
+    def set_shift_x(self, shift):
+        self.shift_x = shift
+        self._reload_tiles()
+
+    def set_shift_y(self, shift):
+        self.shift_y = shift
+        self._reload_tiles()
 
     def _add_button(self, row, col, tile_data=None):
         button = gtk.RadioButton(self._group)
@@ -229,17 +335,51 @@ class LevelEditor(gtk.Window):
         # Sidebar
         self.sidebar = gtk.VBox(False, 6)
         self.sidebar.show()
+        self.sidebar.set_border_width(6)
         hpaned.pack2(self.sidebar)
 
         # Level selector
+        hbox = gtk.HBox(False, 6)
+        hbox.show()
+        self.sidebar.pack_start(hbox, False, False, 0)
+
         self.level_combo = gtk.combo_box_new_text()
         self.level_combo.show()
-        self.sidebar.pack_start(self.level_combo, False, False, 0)
+        hbox.pack_start(self.level_combo, True, True, 0)
+
+        save_button = gtk.Button("Save")
+        save_button.show()
+        hbox.pack_start(save_button, False, False, 0)
+        save_button.connect('clicked', lambda w: self.save())
 
         for level in get_levels():
             self.level_combo.append_text(level.name)
 
         self.level_combo.connect('changed', lambda w: self.load_level())
+
+        # Layer selector
+        self.layer_combo = gtk.combo_box_new_text()
+        self.layer_combo.show()
+        self.sidebar.pack_start(self.layer_combo, False, False, 0)
+        self.layer_combo.append_text('Background Layer')
+        self.layer_combo.append_text('Main Layer')
+        self.layer_combo.append_text('Foreground Layer')
+        self.layer_combo.connect('changed', lambda w: self._on_layer_changed())
+
+        # Shifts
+        hbox = gtk.HBox(False, 12)
+        hbox.show()
+        self.sidebar.pack_start(hbox, False, False, 0)
+
+        self.tile_shift_x = gtk.CheckButton("Shift X")
+        self.tile_shift_x.show()
+        hbox.pack_start(self.tile_shift_x, False, False, 0)
+        self.tile_shift_x.connect('toggled', self._on_shift_x_toggled)
+
+        self.tile_shift_y = gtk.CheckButton("Shift Y")
+        self.tile_shift_y.show()
+        hbox.pack_start(self.tile_shift_y, False, False, 0)
+        self.tile_shift_y.connect('toggled', self._on_shift_y_toggled)
 
         # Sprites list
         swin = gtk.ScrolledWindow()
@@ -265,13 +405,27 @@ class LevelEditor(gtk.Window):
         self.level_grid.show()
         hpaned.pack1(self.level_grid)
 
-
         self.level_combo.set_active(0)
+        self.layer_combo.set_active(2)
 
     def load_level(self):
         loader = LevelLoader(self.level_combo.get_active_text())
 
         self.level_grid.load(loader)
+
+    def save(self):
+        writer = LevelWriter(self.level_combo.get_active_text())
+        self.level_grid.write(writer)
+
+    def _on_layer_changed(self):
+        current_layer = self.layer_combo.get_active()
+        self.level_grid.set_current_layer(current_layer)
+
+    def _on_shift_x_toggled(self, w):
+        self.tile_list.set_shift_x(w.get_active())
+
+    def _on_shift_y_toggled(self, w):
+        self.tile_list.set_shift_y(w.get_active())
 
 
 def main():
---
+++ thecure/levels/writer.py
@@ -0,0 +1,101 @@
+import sys
+
+
+try:
+    from json import dumps
+except ImportError:
+    from simplejson import dumps
+
+from thecure.resources import get_level_filename
+
+
+class LevelWriter(object):
+    def __init__(self, name):
+        self.name = name
+
+    def write(self, layers):
+        files_list = []
+        files_map = {}
+        layer_list = []
+        tile_list = []
+        tile_map = {}
+
+        data = {
+            'files': files_list,
+            'layers': layer_list,
+            'tiles': tile_list,
+        }
+
+        max_width = 0
+        max_height = 0
+
+        for i, layer in enumerate(layers):
+            layer_tiles = []
+
+            layer_data = {
+                'name': layer['name'],
+                'index': i,
+                'is_main': layer['is_main'],
+                'tiles': layer_tiles,
+            }
+            layer_list.append(layer_data)
+
+            rows = layer['tiles']
+            max_height = max(max_height, len(rows))
+
+            for row, row_data in enumerate(rows):
+                row_tiles = []
+
+                col = 0
+
+                num_cols = len(row_data)
+                max_width = max(max_width, num_cols)
+
+                while col < num_cols:
+                    tile_data = row_data[col]
+
+                    if tile_data is None:
+                        col += 1
+                        continue
+
+                    filename = tile_data['filename']
+                    tile_x = tile_data['tile_x']
+                    tile_y = tile_data['tile_y']
+
+                    if filename not in files_map:
+                        file_id = len(files_list)
+                        files_list.append(filename)
+                        files_map[filename] = file_id
+                    else:
+                        file_id = files_map[filename]
+
+                    tile_key = '%s-%s-%s' % (file_id, tile_x, tile_y)
+
+                    if tile_key not in tile_map:
+                        tile_id = len(tile_list)
+                        tile_list.append([file_id, tile_x, tile_y])
+                        tile_map[tile_key] = tile_id
+                    else:
+                        tile_id = tile_map[tile_key]
+
+                    # Figure out the colspan
+                    colspan = 1
+
+                    for endcol in range(col + 1, len(row_data)):
+                        if tile_data == row_data[endcol]:
+                            colspan += 1
+                        else:
+                            break
+
+                    row_tiles.append([tile_id, col, colspan])
+                    col += colspan
+
+                if row_tiles:
+                    layer_tiles.append([row, row_tiles])
+
+        data['width'] = max_width
+        data['height'] = max_height
+
+        fp = open(get_level_filename(self.name), 'w')
+        fp.write(dumps(data))
+        fp.close()
