fc123442727ae25f03c5aa8d2fa7bc6fae388ae2
Christian Hammond <chipx86@chipx86.com>
2012-05-07 03:35:36 -0700

    Move the kids onto the field.

--- thecure/levels/level1.py
+++ thecure/levels/level1.py
@@ -9,13 +9,13 @@ class Level1(Level):
     def setup(self):
         boy = InfectedHuman('boy1')
         self.main_layer.add(boy)
-        boy.move_to(300, 160)
+        boy.move_to(1536, 5696)
         boy.set_direction(Direction.DOWN)
 
         girl = InfectedHuman('girl1')
         self.main_layer.add(girl)
-        girl.move_to(470, 200)
-        girl.set_direction(Direction.LEFT)
+        girl.move_to(1536, 5824)
+        girl.set_direction(Direction.UP)
 
     def draw_bg(self, surface):
         surface.fill((237, 243, 255))
