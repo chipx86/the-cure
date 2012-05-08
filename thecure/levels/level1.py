bf33ea4ac6453bb4850bbc9739be65994fff8343
Christian Hammond <chipx86@chipx86.com>
2012-05-07 17:01:27 -0700

    Add a bunch of NPCs to level 1.

--- thecure/levels/level1.py
+++ thecure/levels/level1.py
@@ -20,6 +20,86 @@ class Level1(Level):
         girl.move_to(1536, 5824)
         girl.set_direction(Direction.UP)
 
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1280, 4800)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1216, 4608)
+        girl.set_direction(Direction.RIGHT)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1152, 3904)
+        girl.set_direction(Direction.RIGHT)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1216, 3136)
+        girl.set_direction(Direction.RIGHT)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(512, 2240)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1408, 2048)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1280, 1152)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(384, 384)
+        girl.set_direction(Direction.RIGHT)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(448, 576)
+        girl.set_direction(Direction.UP)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(960, 128)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(832, 128)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(896, 192)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1600, 576)
+        girl.set_direction(Direction.DOWN)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(320, 1344)
+        girl.set_direction(Direction.RIGHT)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(1152, 2624)
+        girl.set_direction(Direction.UP)
+
+        girl = InfectedHuman('girl1')
+        self.main_layer.add(girl)
+        girl.move_to(512, 4672)
+        girl.set_direction(Direction.RIGHT)
+
         self.eventboxes['kids'].object_entered.connect(
             lambda obj: self.show_monologue_once('kids',
                 'Even the kids are infected. What have I done...'))
